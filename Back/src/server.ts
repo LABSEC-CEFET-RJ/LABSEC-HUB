1904d2195ecb1a9d118ffafb477c0b8e28fa4189
GuilhermeAndradeTaveira <gat3105@gmail.com>
2026-01-19 18:54:58 -0300

    feat:configurações oficiais do servidor node

---
+++ Back/src/server.ts
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config({quiet:true});
+
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+if(!PORT){
+    throw new Error("Variável de ambiente PORT não definida");
+}
+
+app.listen(Number(PORT),()=>{
+    console.log(`Servidor tá up! Porta:${process.env.PORT}`)
+})
+
