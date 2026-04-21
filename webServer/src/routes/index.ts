1973107a07604237b2f219e8a8972fc127f03821
Marco Antonio <marcopcgdev@gmail.com>
2026-04-21 18:00:47 -0300

    cleanup: remove unused userController

--- webServer/src/routes/index.ts
+++ webServer/src/routes/index.ts
@@ -4,17 +4,17 @@ import userRouter from './userRoutes'
 import adminRouter from './adminRoutes'
 import newsRouter from './newsRoutes'
 import vmRouter from './virtual_machine'
-import moduleRouter from './module'
+import moduleRouter from './moduleRoutes'
 import lessonRouter from "./lessonRoutes"
 
 const router = Router()
 
 router.use('/auth', authRouter)
-router.use('/user',userRouter)
+router.use('/user', userRouter)
 router.use('/admin', adminRouter)
 router.use("/news", newsRouter);
 router.use('/vm', vmRouter)
 router.use('/module', moduleRouter)
-router.use('/lesson',lessonRouter)
+router.use('/lesson', lessonRouter)
 
 export default router
